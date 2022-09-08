09986af482b3da1e2d5445ba183a103d18f88865
Martine Grøttum Engen <martge@ntnu.no>
2022-09-08 17:29:02 +0200

    add new files

---
+++ byggern2/sram.c
@@ -0,0 +1,27 @@
+/*
+ * sram.c
+ *
+ * Created: 08.09.2022 15:16:59
+ *  Author: martge
+  
+
+#include "sram.h"
+
+#define BASE_ADDRESS 
+
+void xmem_init ( void ){
+	MCUCR |= (1 << SRE ); // enable XMEM
+	SFIOR |= (1 << XMM0 ); // mask unused bits
+}
+
+void xmem_write ( uint8_t data , uint16_t addr ){
+	volatile char * ext_mem = ( char *) BASE_ADDRESS ;
+	ext_mem [ addr ]= data ;
+}
+
+uint8_t xmem_read (uint16_t addr ){
+	volatile char * ext_mem = ( char *) BASE_ADDRESS ;
+	uint8_t ret_val = ext_mem [ addr ];
+	return ret_val ;
+}
+*/
---
+++ byggern2/sram.h
@@ -0,0 +1,14 @@
+/*
+ * sram.h
+ *
+ * Created: 08.09.2022 15:17:42
+ *  Author: martge
+ */ 
+
+
+/*
+void xmem_init ( void );
+
+void xmem_write ( uint8_t data , uint16_t addr );
+
+uint8_t xmem_read ( uint16_t addr );*/
